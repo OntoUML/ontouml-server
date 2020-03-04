8251adbd297c4270a1ce7eb77908ba6f7c57f847
Claudenir M. Fonseca <claudenirmf@gmail.com>
2020-03-04 17:28:34 +0100

    Updates routes

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import ontoumlRoutes from '@routes/ontouml';
 import notFoundRouter from '@routes/not_found';
 import errorRouter from '@routes/error';
 import { API_VERSION } from '@configs/index';
+import http from 'http';
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+// const port = 80;
 
 app.set('json spaces', 2);
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +21,7 @@ app.use(API_VERSION, ontoumlRoutes);
 app.use(notFoundRouter);
 app.use(errorRouter);
 
-app.listen(port);
+// app.listen(port);
+http.createServer(app).listen(port);
 
 console.log(`API is running on port ${port}`);
--- src/routes/ontouml.ts
+++ src/routes/ontouml.ts
@@ -1,6 +1,12 @@
 import express from 'express';
-import { OntoUMLModel } from 'ontouml-js';
-import BadRequestError from '@error/bad_request';
+// import { OntoUMLModel } from 'ontouml-js';
+import {
+  ModelManager,
+  OntoUML2Verification,
+  OntoUML2GUFO,
+  VerificationIssue,
+} from 'ontouml-js';
+// import BadRequestError from '@error/bad_request';
 
 const router = express.Router();
 
@@ -22,28 +28,140 @@ const router = express.Router();
  *
  */
 router.post(
-  '/verification',
+  '/verify',
   async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction,
   ) => {
-    const model = new OntoUMLModel(req.body);
+    let modelManager: ModelManager;
+    let verification: OntoUML2Verification;
 
-    const errors = await model.verify();
+    try {
+      modelManager = new ModelManager(req.body);
+      verification = new OntoUML2Verification(modelManager);
+      const issues: VerificationIssue[] = await verification.run();
 
-    if (errors.length > 0) {
-      req.app.locals.error = new BadRequestError({
-        title: 'Invalid model',
-        detail: 'The model is not valid OntoUML model',
-        meta: errors,
-      });
+      // console.log(issues);
 
-      next();
-    } else {
-      res.send({ valid: true });
+      if (issues && issues.length > 0) {
+        res.status(200);
+        res.json(JSON.parse(JSON.stringify(issues, replacer)));
+      } else {
+        res.status(200);
+        res.json([]);
+      }
+    } catch (exception) {
+      if (exception.message === 'Invalid model input.') {
+        res.status(400);
+        res.json({
+          status: '400',
+          message: exception.message,
+          errors: exception.errors,
+        });
+      } else {
+        res.status(500).send();
+        console.trace(exception);
+      }
     }
   },
 );
+
+router.post(
+  '/transform/gufo',
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    let modelManager: ModelManager;
+    let service: OntoUML2GUFO;
+
+    try {
+      const model = req.body.model;
+      const options = req.body.options;
+
+      modelManager = new ModelManager(model);
+      service = new OntoUML2GUFO(modelManager);
+      const output = await service.transformOntoUML2GUFO(options);
+
+      res.status(200);
+      if (options.format === 'N-Triples') {
+        res.type('application/n-triples');
+      } else if (options.format === 'N-Quads') {
+        res.type('application/n-quads');
+      } else if (options.format === 'Turtle') {
+        res.type('text/turtle');
+      } else {
+        res.type('text');
+      }
+      res.send(output);
+    } catch (exception) {
+      if (exception.message === 'Invalid model input.') {
+        res.status(400);
+        res.json({
+          status: '400',
+          message: exception.message,
+          errors: exception.errors,
+        });
+      } else {
+        res.status(500).send();
+        console.trace(exception);
+      }
+    }
+  },
+);
+
+// TODO: change this replacer
+function replacer(key, value) {
+  if (key.startsWith('_')) {
+    return undefined;
+  }
+
+  if (this.type) {
+    let contentsFields = [];
+
+    switch (this.type) {
+      case 'Package':
+        contentsFields = ['contents'];
+        break;
+      case 'Class':
+        contentsFields = ['properties', 'literals'];
+        break;
+      case 'Relation':
+        contentsFields = ['properties'];
+        break;
+      case 'Literal':
+        break;
+      case 'Property':
+        break;
+      case 'Generalization':
+        break;
+      case 'GeneralizationSet':
+        // contentsFields = ['generalizations'];
+        break;
+    }
+
+    if (
+      !contentsFields.includes(key) &&
+      key !== 'stereotypes' &&
+      Array.isArray(value)
+    ) {
+      return value.map(item =>
+        item.id && item.type ? { id: item.id, type: item.type } : value,
+      );
+    } else if (
+      !contentsFields.includes(key) &&
+      key !== 'stereotypes' &&
+      value instanceof Object
+    ) {
+      return value.id && value.type
+        ? { id: value.id, type: value.type }
+        : value;
+    }
+  }
+
+  return value;
+}
 
 export default router;
